3aabca3dbe40b82d7516cab48a30116cd86dd6ab
Paul Rony <paul@splendidcrm.com>
2024-02-19 00:57:00 -0500

    Upgrade to React v18

--- Web Site/Services/QueuedBackgroundService.cs
+++ Web Site/Services/QueuedBackgroundService.cs
@@ -26,7 +26,6 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using Spring.Social;
 using System.Diagnostics;
 
 namespace SplendidCRM
