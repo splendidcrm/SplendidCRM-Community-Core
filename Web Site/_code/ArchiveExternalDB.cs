4dd3559761bc814bf3f25798198d20acbb59efc9
Paul Rony <paul@splendidcrm.com>
2026-02-08 23:23:22 -0500

    Update to 15.3

--- Web Site/_code/ArchiveExternalDB.cs
+++ Web Site/_code/ArchiveExternalDB.cs
@@ -63,7 +63,7 @@ namespace SplendidCRM
 			return Strings.Space(nNumber).Replace(' ', '\t');
 		}
 
-		private string CsDataType(string sDATA_TYPE)
+		public string CsDataType(string sDATA_TYPE)
 		{
 			string sCOLUMN_TYPE = String.Empty;
 			switch ( sDATA_TYPE )
@@ -119,7 +119,7 @@ namespace SplendidCRM
 			return sCOLUMN_TYPE;
 		}
 
-		private string BuildArchiveTable(string sTABLE_NAME)
+		public string BuildArchiveTable(string sTABLE_NAME)
 		{
 			sTABLE_NAME = sTABLE_NAME.ToUpper();
 			int nCOLUMN_MAX_LENGTH = 0;
